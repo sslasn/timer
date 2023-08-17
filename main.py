c70210b8bfe6cf49c6f0f25875a60453750ed29d
sslasn <139846977+sslasn@users.noreply.github.com>
2023-08-17 15:52:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+import os
+import platform
+
+def temizle():
+    if platform.system() == "Linux" or platform.system() == "Darwin":
+        os.system("clear")
+    else:
+        os.system("cls")
+while True:
+    zaman = time.localtime()
+    yil = zaman[0]
+    ay = zaman[1]
+    gun = zaman[2]
+    saat = zaman[3]
+    dakika = zaman[4]
+    saniye = zaman[5]
+
+    time.sleep(1)
+    temizle()
+    print("""
+    TARİH:{}/{}/{}
+    SAAT:{}:{}:{}
+    """.format(gun, ay, yil, saat, dakika, saniye))
